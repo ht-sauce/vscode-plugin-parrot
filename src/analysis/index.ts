96fe5c4db9fc58c3d2ba92b34675b9f17f5e6d09
dht <603928588@qq.com>
2023-04-18 14:17:10 +0800

    暂存代码

--- src/analysis/index.ts
+++ src/analysis/index.ts
@@ -2,10 +2,12 @@ import * as eslint from 'eslint'
 import * as fs from 'fs/promises'
 
 // 读取文件内容
-export function readFile(url: string) {
-  console.log(url)
+export async function readFile(url: string) {
+  const fileContent = (await fs.readFile(url)).toString()
+  return fileContent
 }
 
-export function analysis(url: string) {
-  readFile(url)
+export async function analysis(url: string) {
+  const fileStr = await readFile(url)
+  console.log(fileStr)
 }
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,15 @@
 import { analysis } from '../analysis'
+import { join, resolve } from 'path'
 
-analysis('http://localhost:8080')
+async function runAnalysisTest() {
+  try {
+    const rootPath = resolve('.') // 从根目录拼接地址，比较方便
+    const urlStr = join(rootPath + '/test-file/test.vue')
+    console.log(urlStr)
+    // await analysis(urlStr)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+runAnalysisTest()
