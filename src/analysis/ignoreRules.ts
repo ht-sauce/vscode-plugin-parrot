3251af20492f0a86cef3dbdf0b845de417774531
dht <603928588@qq.com>
2023-04-19 17:54:47 +0800

    更新部分功能

--- src/analysis/ignoreRules.ts
+++ src/analysis/ignoreRules.ts
@@ -1,6 +1,6 @@
 import * as EslintPluginVue from 'eslint-plugin-vue'
 import * as VueEslintTsPlugin from '@vue/eslint-config-typescript'
-// 忽略规则的处理
+// 忽略规则的处理，暂时废弃，改用vue-eslint-parser解析
 export function ignoreRules() {
   const vueJsRules = (EslintPluginVue as any).configs['vue3-essential'].rules
   const vueTsRules = (VueEslintTsPlugin as any).overrides[0].rules
--- src/analysis/index.ts
+++ src/analysis/index.ts
@@ -9,6 +9,7 @@ export async function readFile(url: string) {
 
 export async function analysis(url: string) {
   const eslint = new ESLint({
+    fix: false, // 是否自动修复
     overrideConfig: {
       // root: true,
       // 添加vue和ts解析功能
@@ -27,14 +28,15 @@ export async function analysis(url: string) {
         },
         extraFileExtensions: ['.vue'],
       },
-      overrides: [],
       rules: {
         'prettier/prettier': 'off',
       },
     },
   })
 
+  // 检查文件
   const results = await eslint.lintFiles([url])
 
-  console.log(results)
+  // 输出回原文件
+  await ESLint.outputFixes(results)
 }
